8ddfb08be55df7e6651755e73769bc6064edeac9
zzZMH <1273540550@qq.com>
2018-09-05 01:47:11 +0800

    删除敏感信息

--- routes/dbConfig.js
+++ routes/dbConfig.js
@@ -1,9 +1,9 @@
 var config = {
-    host : '60.205.224.2',
-    port : 3306,
-    database : 'test',
-    user : 'root',
-    password : 'ZMH1027@root'
+    host : '',
+    port : ,
+    database : '',
+    user : '',
+    password : ''
 };
 
 module.exports = config;
